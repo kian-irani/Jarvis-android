dd6280a4c3f088039d370789302974edbd040ead
kian-irani <kian.irani.gh@gmail.com>
2026-06-12 14:41:53 +0000

    build: v2.1 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kianirani.jarvis"
         minSdk        = 26
         targetSdk     = 35
-        versionCode   = 2
-        versionName   = "2.0"
+        versionCode   = 3
+        versionName   = "2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GROQ_KEYS", "\"${System.getenv("GROQ_KEYS") ?: ""}\"")
     }
